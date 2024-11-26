7ed8df9eac522308b9ee19eab485c72a9d078273
dragon1204 <168738191+dragon1204@users.noreply.github.com>
2024-11-26 15:41:20 +0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-
-import chesspieces as cp
-board=cp.makeBoard();
-def areBoardsEqual(board1, board2):
-    return all(chess1.name == chess2.name and chess1.location == chess2.location
-               for chess1, chess2 in zip(board1, board2))
-print(len(cp.makeChildrenB(board)))
-    
